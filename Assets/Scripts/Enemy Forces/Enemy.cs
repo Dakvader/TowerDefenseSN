b76c93a024594c81ea512caba232f460a7f63a75
Simon Tiede <simon.tiede@web.de>
2016-09-19 00:51:59 +0200

    WIP Enemy + Tower

---
+++ Assets/Scripts/Enemy Forces/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public double health;
+    public double movementSpeed;
+
+    void damageTaken(int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tower : MonoBehaviour {
+
+    public double range;
+    public double health;
+    public double damage;
+
+    void Update()
+    {
+        List <Enemy> nearbyEnemies = checkForEnemies();
+    }
+
+    List<Enemy> checkForEnemies ()
+    {
+        return null;
+    }
+}
