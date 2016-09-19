eef261968bccf7a8d1337ff0ccd05ed7008d32c3
Simon Tiede <simon.tiede@web.de>
2016-09-19 22:32:47 +0200

    Basic implementation Enemy + Tower

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -4,17 +4,44 @@ using System.Collections.Generic;
 
 public class Tower : MonoBehaviour {
 
-    public double range;
-    public double health;
-    public double damage;
+    public float range;
+    public float health;
+    public float damage = 20;
+
+    List<GameObject> nearbyEnemies = new List<GameObject>();
 
     void Update()
     {
-        List <Enemy> nearbyEnemies = checkForEnemies();
+        if (nearbyEnemies.Count != 0)
+        {
+            int index = Random.Range(0, nearbyEnemies.Count);
+            GameObject enemy = nearbyEnemies[index];
+            enemy.GetComponent<Enemy>().damageTaken(damage);
+
+            Debug.Log("Enemy " + enemy.name + " hit. Health remaining :" + enemy.GetComponent<Enemy>().health);
+        }
     }
 
     List<Enemy> checkForEnemies ()
     {
         return null;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            nearbyEnemies.Add(other.gameObject);
+            Debug.Log(nearbyEnemies.Count);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            nearbyEnemies.Remove(other.gameObject);
+            Debug.Log(nearbyEnemies.Count);
+        }
     }
 }
